618b51edd225b45ca72932a68069d2219c0015c9
DANTA-HOJA <rime97410000@gmail.com>
2022-06-15 20:01:09 +0800

    8051 code, Multi Frequency, concat 2 pin

--- 8051_code/Final_8051_code_MultiFreq.c
+++ 8051_code/Final_8051_code_MultiFreq.c
@@ -9,13 +9,17 @@ void init(void){
 
 int main(void){
 	unsigned char i;
-	int freq_list[2] = {220, 570};
-	int len = sizeof(freq_list)/sizeof(freq_list[0]);
+	
+    // generate frequency = P0 + P1*256, => output = 440, 550, 660 Hz 
+	int freq_list_p0[] = {184, 38, 148};
+	int freq_list_p1[] = {1, 2, 2};
+	int len = sizeof(freq_list_p0)/sizeof(freq_list_p0[0]);
 
 	init();
 	
 	for(i=0;i<len;i++){
-		P0 = freq_list[i]; // target frequency unit Hz.
+		P0 = freq_list_p0[i]; // target frequency unit Hz.
+		P1 = freq_list_p1[i]; // target frequency unit Hz.
 		P3 = 1; // Tell Buzzer to catch value.
 		while(P2 == 0); // Wait for Buzzer process frequency, if done P2 == 1. 
 		P3 = 0; // Send complete, value is not ready.
